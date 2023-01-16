8fc3b0e340e4f5f8f89e1d2ef7a79926e75e9f34
Faran Bhatti <faran_bhatti@hotmail.com>
2023-01-16 14:55:42 -0800

    Added a template for events and tested operation for it

--- app/src/main/java/com/example/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -16,10 +16,69 @@ import android.util.Log;
 
 
 public class MainActivity extends AppCompatActivity {
+    // onCreate event
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Do what you need to do here
         setContentView(R.layout.activity_main);
+
+        // Debugger output
         Log.d("Android", "This is an onCreate event!");
+    }
+    // onStart event
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Do what you need to do here
+
+        // Debugger output
+        Log.d("Android", "This is an onStart event!");
+    }
+    // onResume event
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Do what you need to do here
+
+        // Debugger output
+        Log.d("Android", "This is an onResume event!");
+    }
+    // onPause event
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Do what you need to do here
+
+        // Debugger output
+        Log.d("Android", "This is an onPause event!");
+    }
+
+    // onStop event
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Do what you need to do here
+
+        // Debugger output
+        Log.d("Android", "This is an onStop event!");
+    }
+    // onRestart event
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        // Do what you need to do here
+
+        // Debugger output
+        Log.d("Android", "This is an onRestart event!");
+    }
+    // onDestroy event
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Do what you need to do here
+
+        // Debugger output
+        Log.d("Android", "This is an onDestroy event!");
     }
 }
